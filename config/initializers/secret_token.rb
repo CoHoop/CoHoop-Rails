1109bae0ba0a4b05ce277ada53d6f2b3f58948c0
gabriel-dehan <dehan.gabriel@gmail.com>
2012-07-02 10:07:39 +0200

    Rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CohoopRails::Application.config.secret_token = '2277e13cc18a4d5f3f87b1ae0e14d5dac6b950c4ac834b271d7f10b6a255cdd75ee5d62f0a675e8ff2dacc6e1f89b1ac8e12919c1e176413c030319849fc1555'
