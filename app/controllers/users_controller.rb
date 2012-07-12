2485273228c836531f44e0faf7351ddec9eb8597
gabriel-dehan <dehan.gabriel@gmail.com>
2012-07-12 23:13:11 +0200

    #update now handling avatar uploading through ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update_without_password(params[:user])
-    respond_with (@user) do |f|
-      f.html { redirect_to current_user_profile_path }
+    @user               = User.find(params[:id])
+    is_uploading_avatar = params[:user].delete :avatar_upload_form
+
+    if @user.update_attributes(params[:user])
+      respond_with(@user) do |f|
+        f.html { redirect_to current_user_profile_path }
+
+        if is_uploading_avatar && remotipart_submitted?
+          # As remotipart is only used for the avatar upload here, we can be
+          # confident about the notice.
+          f.js { js_redirect_to(current_user_profile_path) { flash[:notice] = 'Avatar modified successfully.' } }
+        elsif is_uploading_avatar # If the user is submitting a blank upload form
+          f.js
+        end
+      end # respond_with(@user)
+    else
+      respond_with(@user) do |f|
+        f.js if remotipart_submitted?
+      end
     end
   end
 
