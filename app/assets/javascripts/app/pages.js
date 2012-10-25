c55fcbdcfeb5b36cb428f6d3333b1512c689685d
Thibault Duforest <thibault.duforest@gmail.com>
2012-10-25 22:09:53 +0200

    Add flash message style and behaviour

--- app/assets/javascripts/app/pages.js
+++ app/assets/javascripts/app/pages.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  
+  /* Flash message */
+  var flashMessage = $('.flash-messages');
+  
+  $('body').on('click', function(){
+    flashMessage.hide(250);
+  });
+  
+  setTimeout(function(){
+    flashMessage.hide(250);
+    console.log('bye !');
+  }, 4000);
+    
+  
+  
+}());
