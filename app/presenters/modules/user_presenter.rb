7f6c78e3cfc3cb0b301b3e40037194a0e05e8df3
gabriel-dehan <dehan.gabriel@gmail.com>
2012-10-25 15:32:50 +0200

    https://github.com/CoHoop/CoHoop-Rails/issues/29

--- app/presenters/modules/user_presenter.rb
+++ app/presenters/modules/user_presenter.rb
@@ -6,6 +6,10 @@ module UserPresenter
     model.name
   end
 
+  def current_user_profile_link
+    _.profile_path(id: current_user.id, first: current_user.first_name.downcase, last: current_user.last_name.downcase)
+  end
+
   # Public: Creates a thumbnail for the user's avatar
   #
   # Returns an HTML String.
